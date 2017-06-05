1ff3f4bf6c88e6f0e50ce3c6426b2ab9eb37ee09
penndsor <penndsor@beathap.reclaimhosting.com>
2017-06-05 12:46:21 -0400

    test commit

--- common/header.php
+++ common/header.php
@@ -21,40 +21,38 @@
     <!-- Plugin Stuff -->
     <?php fire_plugin_hook('public_head', array('view'=>$this)); ?>
 
+
     <!-- Stylesheets -->
+    <!-- Le styles -->
     <?php
-    queue_css_url('//fonts.googleapis.com/css?family=Ubuntu:300,400,500,700,300italic,400italic,500italic,700italic');
-    queue_css_file(array('iconfonts', 'normalize', 'style'), 'screen');
-    queue_css_file('print', 'print');
+    queue_css_url('http://fonts.googleapis.com/css?family=Oxygen');
+    queue_css_url('http://fonts.googleapis.com/css?family=Inconsolata');
+    queue_css_file(array(
+        'bootstrap',
+        'font-awesome',
+        'style',
+    ));
     echo head_css();
     ?>
-    
-    <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    
-    <!-- jQuery style for Tooltips -->
-<link href="https://code.jquery.com/ui/1.11.0/themes/smoothness/jquery-ui.css" rel="stylesheet" type="text/css" />
 
     <!-- JavaScripts -->
-    <?php 
+    <?php
+    // jQuery is enabled by default in Omeka and in most themes.
+    // queue_js_url('https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js');
+    // queue_js_url('https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js');
     queue_js_file(array(
-        'vendor/selectivizr',
-        'vendor/jquery-accessibleMegaMenu',
-        'vendor/respond',
-        'jquery-extra-selectors',
-        'seasons',
-        'globals',
-        'jquery.bxSlider.min'
-    )); 
+        'bootstrap.min',
+        'jquery.bxSlider.min',
+    ));
+    echo head_js();
     ?>
-
-    <?php echo head_js(); ?>
 </head>
+
 <?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
    
     <?php fire_plugin_hook('public_body', array('view'=>$this)); ?>
      
-     <button type="button" href="#content" id="skipnav" class="btn btn-danger">Skip to main content</button>
+   <!--  <button type="button" href="#content" id="skipnav" class="btn btn-danger">Skip to main content</button> -->
     <div id="wrap">
         <header role="banner">
             <div id="site-title" href="http://pennds.org/archaebot_database/">
