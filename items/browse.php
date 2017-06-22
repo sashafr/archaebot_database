357b7189d4b75aee639b8e9921ce2f666105f7b2
penndsor <penndsor@beathap.reclaimhosting.com>
2017-06-22 16:54:29 -0400

    cosmetic changes to user register and login pages, added styles to taxonomy pages

--- items/browse.php
+++ items/browse.php
@@ -5,14 +5,15 @@ echo head(array('title'=>$pageTitle,'bodyclass' => 'items browse'));
 
 <h1><?php echo $pageTitle;?> <?php echo __('(%s total)', $total_results); ?></h1>
 
+<?php echo $this->partial('items/search-form.php',
+    array('formAttributes' =>
+        array('id'=>'advanced-search-form'))); ?>
 
-<nav class="items-nav navigation secondary-nav">
-    <?php echo public_nav_items(); ?>
-</nav>
+<?php // echo item_search_filters(); ?>
 
-<?php echo item_search_filters(); ?>
+<?php // echo pagination_links(); ?>
 
-<?php echo pagination_links(); ?>
+<?php $posters = $this-> posters; //from posters ?> 
 
 <?php if ($total_results > 0): ?>
 
@@ -21,13 +22,15 @@ $sortLinks[__('Title')] = 'Dublin Core,Title';
 $sortLinks[__('Creator')] = 'Dublin Core,Creator';
 $sortLinks[__('Date Added')] = 'added';
 ?>
-<div id="sort-links">
+
+<div id="sort-links"> <!--class="outputs" style="list-style:none;-->
     <span class="sort-label"><?php echo __('Sort by: '); ?></span><?php echo browse_sort_links($sortLinks); ?>
 </div>
 
 <?php endif; ?>
 
 <?php foreach (loop('items') as $item): ?>
+
 <div class="item hentry">
     <h2><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class'=>'permalink')); ?></h2>
     <div class="item-meta">
@@ -48,7 +51,7 @@ $sortLinks[__('Date Added')] = 'added';
         <?php echo tag_string('items'); ?></p>
     </div>
     <?php endif; ?>
-
+<!-- <a href="<?php echo public_url(array('controller'=> 'cart', 'action' => 'add')); ?>" class="btn btn-success">Add to Cart</a> -->
     <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
 
     </div><!-- end class="item-meta" -->
--- items/show.php
+++ items/show.php
@@ -20,11 +20,13 @@
         <?php echo item_image_gallery(); ?>
     </div>
     <?php endif; ?>
+    
+  <!-- <hr style="color: black; border: 0; border-top: 3px double #8c8c8c;"> -->
 
     <!-- If the item belongs to a collection, the following creates a link to that collection. -->
     <?php if (metadata('item', 'Collection Name')): ?>
     <div id="collection" class="element">
-        <h2><?php echo __('Collection'); ?></h2>
+        <h3><?php echo __('Collection'); ?></h3>
         <div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
     </div>
     <?php endif; ?>
@@ -32,14 +34,14 @@
     <!-- The following prints a list of all tags associated with the item -->
     <?php if (metadata('item', 'has tags')): ?>
     <div id="item-tags" class="element">
-        <h2><?php echo __('Tags'); ?></h2>
+        <h3><?php echo __('Tags'); ?></h3>
         <div class="element-text"><?php echo tag_string('item'); ?></div>
     </div>
     <?php endif;?>
 
     <!-- The following prints a citation for this item. -->
     <div id="item-citation" class="element">
-        <h2><?php echo __('Citation'); ?></h2>
+        <h3><?php echo __('Citation'); ?></h3>
         <div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
     </div>
 </aside>
